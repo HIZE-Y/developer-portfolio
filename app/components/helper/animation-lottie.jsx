256a0aebe351b6f29f57501c0d59addc35a4513a
hize-y <yahyahiza@gmail.com>
2025-03-18 19:07:38 -0400

    3

--- app/components/helper/animation-lottie.jsx
+++ app/components/helper/animation-lottie.jsx
@@ -1,8 +1,23 @@
 "use client"
 
-import Lottie from "lottie-react";
+import dynamic from 'next/dynamic';
+import { useState, useEffect } from 'react';
+
+const Lottie = dynamic(() => import('lottie-react'), {
+  ssr: false,
+});
 
 const AnimationLottie = ({ animationPath, width }) => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return null;
+  }
+
   const defaultOptions = {
     loop: true,
     autoplay: true,
